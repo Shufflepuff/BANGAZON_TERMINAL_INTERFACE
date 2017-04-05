5d8ae3689a84bcfbd22c921f4e2fd5f5cc01a92b
Alex Saber <alexsaber89@gmail.com>
2017-04-05 18:04:31 -0500

    add columns to OrderLine model

--- Shufflepuff-Solution/Shufflepuff-ConsoleApp/Models/OrderLine.cs
+++ Shufflepuff-Solution/Shufflepuff-ConsoleApp/Models/OrderLine.cs
@@ -8,5 +8,12 @@ namespace Shufflepuff_ConsoleApp.Models
 {
     public class OrderLine
     {
+        public int OrderLineId { get; set; }
+
+        public int InvoiceId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
