e813f160a8b4306d3e655b15574ede09744049c5
Stephen Szpak <srszpak@gmail.com>
2017-04-04 20:52:14 -0500

    opening credits

--- Shufflepuff-Solution/Shufflepuff-ConsoleApp/Program.cs
+++ Shufflepuff-Solution/Shufflepuff-ConsoleApp/Program.cs
@@ -10,6 +10,14 @@ namespace Shufflepuff_ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"
+            ************************************
+            ******* SHUFFLEPUFF BANGAZON *******
+            ************************************");
+            Console.WriteLine(" 1. Create an Account" + Environment.NewLine + " 2. Choose Active Customer" + Environment.NewLine + " 3. Create Payment Option" + Environment.NewLine + " 4. Search for Products" + Environment.NewLine + " 5. Complete Order" + Environment.NewLine + " 6. See Product Popularity" + Environment.NewLine + " 7. Leave Shufflepuff Bangazon");
+            Console.ReadLine();
+
+
         }
     }
 }
