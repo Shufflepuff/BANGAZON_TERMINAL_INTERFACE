617cb0068576461e2b039409fd00b008f0cd6523
Taylor Shuff <rhd5538@trh.com>
2017-04-07 10:59:27 -0500

    established SqlConnection.

--- Shufflepuff-Solution/Shufflepuff-ConsoleApp/Repository/OrderLineRepo.cs
+++ Shufflepuff-Solution/Shufflepuff-ConsoleApp/Repository/OrderLineRepo.cs
@@ -1,14 +1,24 @@
 ﻿using Shufflepuff_ConsoleApp.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
 
 namespace Shufflepuff_ConsoleApp.Repository
 {
     public class OrderLineRepo : IOrderLineRepo
     {
+        SqlConnection _DbConnection;
+
+        public OrderLineRepo()
+        {
+            _DbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ShufflepuffBang"].ConnectionString);
+        }
         public void AddOrderLine()
         {
             throw new NotImplementedException();
