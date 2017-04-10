6960b3832a36c0fd751635953f45c5be1cb482dd
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-04-10 18:06:33 -0500

    added class for complete order.

---
+++ Shufflepuff-Solution/Shufflepuff-ConsoleApp/UI/CompleteOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shufflepuff_ConsoleApp.UI
+{
+    class CompleteOrder
+    {
+        //If no products have been selected
+        //Please add some products to your order first. Press any key to return to main menu.
+
+        //if there are products in order
+        //Your order total is $149.54. Ready to purchase
+        //(Y/N) >
+        //# If user entered Y
+        //        Choose a payment option
+        //1. Amex
+        //2. Visa
+        //>
+
+        //Your order is complete! Press any key to return to main menu.
+
+        //# If user entered N, display the main menu again
+        //Once the order is complete, show the main menu again, where the user can start creating another order.
+    }
+}
